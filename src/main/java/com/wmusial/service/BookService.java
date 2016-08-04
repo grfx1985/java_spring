28deb0e957d20ab8f8190a9c87d7bb6fdd2cee89
Grfx <michalandros@gmail.com>
2016-08-04 15:16:07 +0200

    added @services to services

--- src/main/java/com/wmusial/service/BookService.java
+++ src/main/java/com/wmusial/service/BookService.java
@@ -1,9 +1,10 @@
 package com.wmusial.service;
 
 import com.wmusial.model.Book;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface BookService {
     List<Book> findAll();
 
--- src/main/java/com/wmusial/service/impl/BookServiceImpl.java
+++ src/main/java/com/wmusial/service/impl/BookServiceImpl.java
@@ -5,9 +5,10 @@ import com.wmusial.dao.BookRepository;
 import com.wmusial.model.Book;
 import com.wmusial.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
