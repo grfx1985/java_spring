d6739131662f0174769ba8b7571d643129c96134
Grfx <michalandros@gmail.com>
2016-08-04 13:48:58 +0200

    adjustet User class to work with Config

--- src/main/java/com/wmusial/model/User.java
+++ src/main/java/com/wmusial/model/User.java
@@ -1,5 +1,22 @@
 package com.wmusial.model;
 
-public class User extends BaseEntity {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor // wszystkie konstruktory
+@NoArgsConstructor // konstruktor bazowy pusty
+@Entity
+@Table(name="user")
+
+public @Data class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 
 }
