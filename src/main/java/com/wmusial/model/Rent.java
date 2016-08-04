830d208450ccfecce10772f2ac3458458991c83f
Grfx <michalandros@gmail.com>
2016-08-04 13:54:43 +0200

    adnotations for rent class

--- src/main/java/com/wmusial/model/Rent.java
+++ src/main/java/com/wmusial/model/Rent.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 @AllArgsConstructor // wszystkie konstruktory
 @NoArgsConstructor // konstruktor bazowy pusty
@@ -13,4 +14,5 @@ import javax.persistence.Table;
 @Table(name="rent")
 
 public @Data class Rent extends BaseEntity {
+    private Date returnDate;
 }
