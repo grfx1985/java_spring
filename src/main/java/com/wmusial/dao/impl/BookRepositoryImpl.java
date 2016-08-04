c0dcacfe84029b331d2167db0064eb8e8b98cd31
Grfx <michalandros@gmail.com>
2016-08-04 15:05:12 +0200

    Book Repository and its implementation

--- src/main/java/com/wmusial/dao/impl/BookRepositoryImpl.java
+++ src/main/java/com/wmusial/dao/impl/BookRepositoryImpl.java
@@ -4,12 +4,23 @@ import com.wmusial.dao.BookRepository;
 import com.wmusial.model.Book;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Book> findAll() {
-        return null;
+        TypedQuery<Book> query = entityManager.createQuery("SELECT b FROM Book b", Book.class);
+
+        List<Book> books = query.getResultList();
+
+        return books;
     }
 }
