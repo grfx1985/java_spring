f7119c0b6a208c5ef0832c9f1e7ab9199e3936fb
Grfx <michalandros@gmail.com>
2016-08-04 13:51:45 +0200

    adnotations for rent class

--- src/main/java/com/wmusial/model/Rent.java
+++ src/main/java/com/wmusial/model/Rent.java
@@ -1,4 +1,16 @@
 package com.wmusial.model;
 
-public class Rent extends BaseEntity {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor // wszystkie konstruktory
+@NoArgsConstructor // konstruktor bazowy pusty
+@Entity
+@Table(name="rent")
+
+public @Data class Rent extends BaseEntity {
 }
