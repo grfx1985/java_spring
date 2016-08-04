bc0d63826a4fc21bd54d0dbb6823109edb34ff9a
Grfx <michalandros@gmail.com>
2016-08-04 16:00:44 +0200

    example sping data querry methods

--- src/main/java/com/wmusial/dao/BookRepository.java
+++ src/main/java/com/wmusial/dao/BookRepository.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
+    List<Book> findByAuthor(String author);
+    List<Book> findByAvailableLessThan(int available);
+
+
 
 //    List<Book> findAll();
 }
