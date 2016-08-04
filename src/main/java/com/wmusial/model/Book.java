7c9227d0a460e6a3c23a6460137ce554fd7ee88e
Grfx <michalandros@gmail.com>
2016-08-04 13:41:24 +0200

    entity added to Book

--- src/main/java/com/wmusial/model/Book.java
+++ src/main/java/com/wmusial/model/Book.java
@@ -4,9 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 
 @AllArgsConstructor // wszystkie konstruktory
 @NoArgsConstructor // konstruktor bazowy pusty
+@Entity
+@Table(name="book")
 public @Data class Book extends BaseEntity {
 
     private String title;
