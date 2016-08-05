c30c41d1d89aca53b0be8bad53fa28cb34a5bea2
Grfx <michalandros@gmail.com>
2016-08-05 17:18:21 +0200

    rent class

--- src/main/java/com/wmusial/model/Rent.java
+++ src/main/java/com/wmusial/model/Rent.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @AllArgsConstructor // wszystkie konstruktory
@@ -14,6 +13,22 @@ import java.util.Date;
 @Table(name="rent")
 
 public @Data class Rent extends BaseEntity {
+
+    public enum Status { IN_PROGRESS , FINISHED }
+    @Column(name="return_date")
     private Date returnDate;
+    @Column(name="rent_date")
     private Date rentDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="book_id")
+    private Status book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.IN_PROGRESS;
 }
