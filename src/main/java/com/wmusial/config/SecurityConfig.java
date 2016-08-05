c1a758024652a570c8be6a05ba1471f8ffc30f7c
Grfx <michalandros@gmail.com>
2016-08-05 11:23:17 +0200

    configuring web security steps - 4 using jdbc option

--- src/main/java/com/wmusial/config/SecurityConfig.java
+++ src/main/java/com/wmusial/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.wmusial.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 // konfiguracja zabezpieczenia aplikacji logowanie itd
 @Configuration
@@ -11,8 +14,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // ctrl + o i wybieramy configura....
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // auth.inMemoryAuthentication().withUser("admin").password("1234").roles("ADMIN"); // developement login option
+
+        // auth.jdbcAuthentication().dataSource(dataSource); // uzywanie jdbc standard java  pobiera autowire z hibernate config.
+        // spring zaklada ze mamy users table z username  i password kolumnami
     }
 }
