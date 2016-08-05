80cbce7007f869924874f9eaf620b871a56bd94f
Grfx <michalandros@gmail.com>
2016-08-05 16:45:09 +0200

    new pom thing to work with json response

--- src/main/java/com/wmusial/controller/rest/RestUserController.java
+++ src/main/java/com/wmusial/controller/rest/RestUserController.java
@@ -3,9 +3,11 @@ package com.wmusial.controller.rest;
 import com.wmusial.model.User;
 import com.wmusial.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,7 +18,14 @@ public class RestUserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value="/users" , method= RequestMethod.GET)
+    @RequestMapping(value="/users" , method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    // trzeba dolozyc do poma biblioteke
+    /*  <dependency>
+            <groupId>com.fasterxml.jackson.core</groupId>
+            <artifactId>jackson-databind</artifactId>
+            <version>2.7.3</version>
+        </dependency>*/
     public List<User> getUsers() {
 
         List<User> users = userService.findAll();
