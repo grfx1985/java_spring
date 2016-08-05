4115fab560985a2bb7740e2e3da5c149c15292f6
Grfx <michalandros@gmail.com>
2016-08-05 16:22:53 +0200

    security adjustement

--- src/main/java/com/wmusial/config/SecurityConfig.java
+++ src/main/java/com/wmusial/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/resources/**").permitAll()
                 .antMatchers("/login").permitAll()
+                .antMatchers("/users","users/**" , "/admin/**").hasRole("ADMIN") // wszystko po sciezkach dostepne tylko dla admina
                 .antMatchers("/register").permitAll()
                 .antMatchers("/**").authenticated()
 
