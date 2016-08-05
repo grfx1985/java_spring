db7c7a45293a0b879e4fbf95bf8d79419900fd1c
Grfx <michalandros@gmail.com>
2016-08-05 15:35:33 +0200

    roles in User via enums

--- src/main/java/com/wmusial/model/User.java
+++ src/main/java/com/wmusial/model/User.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor // wszystkie konstruktory
 @NoArgsConstructor // konstruktor bazowy pusty
@@ -18,9 +16,16 @@ public @Data class User extends BaseEntity {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    
+
     @Column(name="email", unique = true)
     private String email;
     private String password;
+
+    // role uzytkownika
+
+    public enum Role { ADMIN , USER }
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER; // 0 - admin , 1 - user
 
 }
