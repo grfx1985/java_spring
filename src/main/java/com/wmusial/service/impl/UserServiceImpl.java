e9297d723a521114becdb7786637c73088f441c3
Grfx <michalandros@gmail.com>
2016-08-05 11:29:08 +0200

    configuring web security steps - 5 using spring hibernate services

--- src/main/java/com/wmusial/service/impl/UserServiceImpl.java
+++ src/main/java/com/wmusial/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.wmusial.service.impl;
 
 import com.wmusial.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service  // dla opcji logowania dodajemy zeby config czytal
 public class UserServiceImpl implements UserService {
+                // alt + enter
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException { // logujemy sie emailem - zmianiamy "s"
 
+        return null;
+    }
 }
