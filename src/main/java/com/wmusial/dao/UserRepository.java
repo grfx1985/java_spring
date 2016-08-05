f6890db2fe52462984a178654f9986b0d03876e1
Grfx <michalandros@gmail.com>
2016-08-05 11:36:13 +0200

    configuring web security steps - 5 using spring hibernate services

--- src/main/java/com/wmusial/dao/UserRepository.java
+++ src/main/java/com/wmusial/dao/UserRepository.java
@@ -1,6 +1,12 @@
 package com.wmusial.dao;
 
+import com.wmusial.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    User findByEmail(String email);
 
 }
--- src/main/java/com/wmusial/service/impl/UserServiceImpl.java
+++ src/main/java/com/wmusial/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.wmusial.service.impl;
 
+import com.wmusial.dao.UserRepository;
+import com.wmusial.model.User;
 import com.wmusial.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -9,8 +12,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
                 // alt + enter
 
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException { // logujemy sie emailem - zmianiamy "s"
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Nie znaleziono Uzytkownika " + email );
+        }
 
         return null;
     }
