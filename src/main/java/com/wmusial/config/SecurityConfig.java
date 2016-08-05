49f8e3b8d0902c51f03e3cbb82a58c0425630249
Grfx <michalandros@gmail.com>
2016-08-05 12:08:14 +0200

    configuring web security steps - 6 getting the right views to work with spring security

--- src/main/java/com/wmusial/config/SecurityConfig.java
+++ src/main/java/com/wmusial/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.wmusial.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -36,4 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.userDetailsService(userService); // opcja z frameworkiem spring hibernate 3 opcja
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // dopasowanie odpowiednich widokow nie default springowy ekran logowania
+
+    }
 }
+
