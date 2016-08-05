c1370bfe7e433943e0e9fcdb45b17b705b1643d0
Grfx <michalandros@gmail.com>
2016-08-05 11:03:31 +0200

    configuring web security steps - 1

---
+++ src/main/java/com/wmusial/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.wmusial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+// configuracja zabezpieczenia aplikacji logowanie itd
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+}
